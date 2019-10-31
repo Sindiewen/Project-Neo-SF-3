a6b5455d76872a26085ae0c70dab0e69c198bb33
Rachel Vancleave <sindiewen@gmail.com>
2019-10-30 20:42:27 -0700

    Started building player input controller: - Player physics will be handled via Rigidbody 2D, and controling the player through modifying the position of the rigidbody.

---
+++ Neo SF-3/Assets/_Scripts/Actors/Player/PlayerMovement.cs
@@ -0,0 +1,68 @@
+﻿using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    // variables
+    public float moveSpeed = 5f;
+
+
+
+    // Private variables
+
+    // Player movement variables
+    private Vector2 moveDirection;
+
+    // Component references
+    private Rigidbody2D rb2d;       // Reference to the player rigidbody component   
+
+
+    private void Start()
+    {
+        // Initialize components
+        rb2d = GetComponent<Rigidbody2D>();
+    }
+
+
+    /// <summary>
+    /// UPdates every frame
+    /// </summary>
+    private void Update()
+    {
+        // Gets player movement
+        getInput();
+    }
+
+    /// <summary>
+    /// Fixed update function
+    /// runs every frame, but executed on a fixed timer
+    /// </summary>
+    private void FixedUpdate()
+    {
+        initiatiteMovement();
+    }
+    
+
+
+    // Player Movement
+    // ----------------------------------------------------------
+
+    /// <summary>
+    /// Gets any input from the player
+    /// </summary>
+    private void getInput()
+    {
+        // Get horizontal and vertical input
+        moveDirection.x = Input.GetAxisRaw("Horizontal");
+        moveDirection.y = Input.GetAxisRaw("Vertical");
+    }
+
+    private void initiatiteMovement()
+    {
+        // Move player position based on the current player position + the direction the playe ris moving,
+        // Then multiply that by the speed the player will move, and then multiplay that by deltaTime to ensure
+        // it's moving at realtime
+        rb2d.MovePosition(rb2d.position + moveDirection * moveSpeed * Time.fixedDeltaTime);
+    }
+
+}
