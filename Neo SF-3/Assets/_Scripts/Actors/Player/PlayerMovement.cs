50bd7d1bf6ea11b9c20fb4a647461945bea2c8ec
Rachel Vancleave <sindiewen@gmail.com>
2019-11-01 14:24:57 -0700

    Started building player Attributes and building up the movement controller

--- Neo SF-3/Assets/_Scripts/Actors/Player/PlayerMovement.cs
+++ Neo SF-3/Assets/_Scripts/Actors/Player/PlayerMovement.cs
@@ -3,34 +3,97 @@
 public class PlayerMovement : MonoBehaviour
 {
 
+    // Enums
+    #region enums
+    
+    // Defines which player this player is
+    public enum PLAYER_NUMBER
+    {
+        PLAYER_ONE,
+        PLAYER_TWO
+    };
+
+
+    #endregion
+
     // variables
-    public float moveSpeed = 5f;
+    #region Variables
+
+    // public Variables
+    // ---------------------------------------------
+    [Header("Player Number")]
+    public PLAYER_NUMBER player_number; // Defines which number the player is
+
+    [Header("Player Movement Values")]
+    public float moveSpeed = 5f;        //How fast the player will move
+
+    [Header("InputManager References")]
+    public string moveHorizontal = "Horizontal";
+    public string moveVertical = "Vertical";
+    public string attackKey = "Fire1";
 
 
 
     // Private variables
-
-    // Player movement variables
-    private Vector2 moveDirection;
+    // ---------------------------------------------
 
     // Component references
-    private Rigidbody2D rb2d;       // Reference to the player rigidbody component   
+    private Rigidbody2D rb2d;           // Reference to the player rigidbody component   
 
 
+    // Player input data
+    private Vector2 moveDirection;
+    private bool isAttacking;
+
+
+    #endregion
+
+
+    #region Private Methods
+
+
+    /// <summary>
+    /// Unity start method
+    /// 
+    /// Runs once when the scene starts
+    /// </summary>
     private void Start()
     {
         // Initialize components
         rb2d = GetComponent<Rigidbody2D>();
+
+        // Initialize the player's input values
+        // If the player is player two, append "_p2" at the end to say it's a player 2
+        if (player_number == PLAYER_NUMBER.PLAYER_TWO)
+        {
+            moveHorizontal += "_p2";
+            moveVertical += "_p2";
+            attackKey += "_p2";
+        }
+        // Player 1 = "_p1"
+        else
+        {
+            moveHorizontal += "_p1";
+            moveVertical += "_p1";
+            attackKey += "_p1";
+        }
+
     }
 
 
     /// <summary>
+    /// Unity update method
+    ///
     /// UPdates every frame
     /// </summary>
     private void Update()
     {
         // Gets player movement
         getInput();
+
+        // Initiate player input
+        // ----------------------------
+        initiateAttack();
     }
 
     /// <summary>
@@ -39,24 +102,40 @@ public class PlayerMovement : MonoBehaviour
     /// </summary>
     private void FixedUpdate()
     {
+        // Iniitate player movement (NOTE: Unity Physics must be kept inside of FixedUpdate()
+        // to ensure physics are not tied to the frame rate)
         initiatiteMovement();
     }
-    
 
 
-    // Player Movement
-    // ----------------------------------------------------------
+
+
+    // Parse playerInput
+    // ------------------------------------------------------------
 
     /// <summary>
     /// Gets any input from the player
     /// </summary>
     private void getInput()
     {
+
+        // Move the player
         // Get horizontal and vertical input
-        moveDirection.x = Input.GetAxisRaw("Horizontal");
-        moveDirection.y = Input.GetAxisRaw("Vertical");
+        moveDirection.x = Input.GetAxisRaw(moveHorizontal);
+        moveDirection.y = Input.GetAxisRaw(moveVertical);
+
+        // check for attackInput
+        // If the player presses the attack key, set attacking to true
+        isAttacking = Input.GetButtonDown(attackKey);
     }
 
+
+    // Initiate player Input
+    // ----------------------------------------------------------
+
+    /// <summary>
+    /// Initiates player movement
+    /// </summary>
     private void initiatiteMovement()
     {
         // Move player position based on the current player position + the direction the playe ris moving,
@@ -64,5 +143,20 @@ public class PlayerMovement : MonoBehaviour
         // it's moving at realtime
         rb2d.MovePosition(rb2d.position + moveDirection * moveSpeed * Time.fixedDeltaTime);
     }
+
+    /// <summary>
+    /// Initiates player attack
+    /// </summary>
+    private void initiateAttack()
+    {
+        // If the player has pressed the attack key on the same frame
+        if (isAttacking)
+        {
+            // Initiate the player attack
+
+        }
+    }
+
+    #endregion
 
 }
